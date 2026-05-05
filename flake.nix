416f64c2fff4ec5d72cbe5b9692c44c6729d92e5
Noon van der Silk <noon.vandersilk@iohk.io>
2026-05-05 14:52:40 +0100

    new nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/25.11";
     gomod2nix = {
       url = "github:nix-community/gomod2nix";
       inputs.nixpkgs.follows = "nixpkgs";
